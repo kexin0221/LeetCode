7591295fa28c0fbab8b7932fc589965c4be1f2b1
mou-kexin <2584857586@qq.com>
2025-11-19 09:17:07 +0800

    力扣代码

--- leetcode/editor/cn/[26]删除有序数组中的重复项.java
+++ leetcode/editor/cn/[26]删除有序数组中的重复项.java
@@ -1,11 +1,24 @@
 package leetcode.editor.cn;
 
-import java.util.HashMap;
-
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution26 {
     public int removeDuplicates(int[] nums) {
-
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        int len = nums.length;
+        int p = 0, q = 1;
+        while (q < len) {
+            if (nums[p] != nums[q]) {
+                nums[p + 1] = nums[q];
+                p++;
+            }
+            q++;
+        }
+        return p+1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
