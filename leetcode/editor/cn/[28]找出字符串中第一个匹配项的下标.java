65096c1c2375990a540c9aa531c362d861b4d9b1
mou-kexin <2584857586@qq.com>
2025-11-21 10:00:10 +0800

    力扣代码

--- leetcode/editor/cn/[28]找出字符串中第一个匹配项的下标.java
+++ leetcode/editor/cn/[28]找出字符串中第一个匹配项的下标.java
@@ -3,7 +3,25 @@ package leetcode.editor.cn;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int strStr(String haystack, String needle) {
-        
+        if (haystack == null || needle == null) {
+            return -1;
+        }
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                int hayIndex = i;
+                int needleIndex = 0;
+                while (needleIndex < needle.length()) {
+                    if (haystack.charAt(hayIndex) != needle.charAt(needleIndex)) {
+                        return -1;
+                    } else {
+                        hayIndex++;
+                        needleIndex++;
+                    }
+                }
+                return i;
+            }
+        }
+        return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
