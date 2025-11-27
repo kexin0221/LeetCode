1bd0b072fcd6f0c2998d8c27598dc3ca694946e8
mou-kexin <2584857586@qq.com>
2025-11-27 11:06:20 +0800

    力扣代码

--- leetcode/editor/cn/[58]最后一个单词的长度.java
+++ leetcode/editor/cn/[58]最后一个单词的长度.java
@@ -1,7 +1,7 @@
 package leetcode.editor.cn;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution58 {
     public int lengthOfLastWord(String s) {
         String[] strArr = s.split(" ");
         return strArr[strArr.length - 1].length();
