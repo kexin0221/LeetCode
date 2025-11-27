1f775104b677131f4435bb56d77a3a8a2e9e4362
mou-kexin <2584857586@qq.com>
2025-11-27 11:20:25 +0800

    力扣代码

--- leetcode/editor/cn/[66]加一.java
+++ leetcode/editor/cn/[66]加一.java
@@ -1,12 +1,19 @@
 package leetcode.editor.cn;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] plusOne(int[] digits) {
-        if (digits[digits.length - 1] != 9) {
-            digits[digits.length - 1]++;
-            return digits;
+        String string = Arrays.toString(digits);
+        int num = Integer.getInteger(string) + 1;
+        String newStr = String.valueOf(num);
+        int[] ret = new int[newStr.length()];
+        for (int i = 0; i < newStr.length(); i++) {
+            ret[i] = Character.getNumericValue(newStr.charAt(i));
         }
+        return ret;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
