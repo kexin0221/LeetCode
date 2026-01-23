d3f4d78814993eb8908b37fb3bb39426c121a061
mou-kexin <2584857586@qq.com>
2026-01-23 21:43:52 +0800

    力扣代码

--- leetcode/editor/cn/[611]有效三角形的个数.java
+++ leetcode/editor/cn/[611]有效三角形的个数.java
@@ -3,24 +3,24 @@ package leetcode.editor.cn;
 import java.util.Arrays;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution611 {
     public int triangleNumber(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
-        int left = 0, right = n - 1;
+        int i = n - 1;
         int count = 0;
-        while (right > 1) {
-            int maxLength = nums[right];
-            while (left + 1 < right) {
-                for (int i = 1; i < right - left; i++) {
-                    if (nums[left] + nums[left+i] > maxLength) {
-                        count++;
-                    }
+        while (i > 1) {
+            int maxLength = nums[i];
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > maxLength) {
+                    count += right - left;
+                    right--;
+                } else {
+                    left++;
                 }
-                left++;
             }
-            right--;
-            left = 0;
+            i--;
         }
         return count;
     }
