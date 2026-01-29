bdc5d74c4ba431264106eee2b64f18466186f69d
mou-kexin <2584857586@qq.com>
2026-01-29 22:17:50 +0800

    力扣代码

--- leetcode/editor/cn/[209]长度最小的子数组.java
+++ leetcode/editor/cn/[209]长度最小的子数组.java
@@ -5,30 +5,15 @@ import java.util.Arrays;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int minLength = nums.length;
-        if (arrSum(nums) < target) return 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) return 1;
-            for (int j = i; j < nums.length; j++) {
-                int[] newArr = Arrays.copyOfRange(nums, i, j+1);
-                if (arrSum(newArr) >= target && newArr.length < minLength) {
-                    minLength = newArr.length;
-                    break;
-                }
-//                if (nums[i] + nums[j] > target) {
-//                    break;
-//                }
+        int minLength = Integer.MAX_VALUE, n = nums.length, sum = 0;
+        for (int left = 0, right = 0; right < n; right++) {
+            sum += nums[right];
+            while (sum >= target) {
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left++];
             }
         }
-        return minLength;
-    }
-
-    private static int arrSum (int[] arr) {
-        int sum = 0;
-        for (int num : arr) {
-            sum += num;
-        }
-        return sum;
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
