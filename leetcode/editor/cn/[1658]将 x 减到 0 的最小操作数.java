fd950f138cf422c286a71535127c86866a8a76e1
mou-kexin <2584857586@qq.com>
2026-02-02 21:44:08 +0800

    力扣代码

--- leetcode/editor/cn/[1658]将 x 减到 0 的最小操作数.java
+++ leetcode/editor/cn/[1658]将 x 减到 0 的最小操作数.java
@@ -3,7 +3,7 @@ package leetcode.editor.cn;
 import java.util.Arrays;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution1658 {
     public int minOperations(int[] nums, int x) {
         int target = Arrays.stream(nums).sum() - x;
         if (target < 0) return -1;
