ea443c38c64a23439897a8095b58b0b8ccd58b3e
mou-kexin <2584857586@qq.com>
2026-02-04 22:09:16 +0800

    力扣代码

--- leetcode/editor/cn/[904]水果成篮.java
+++ leetcode/editor/cn/[904]水果成篮.java
@@ -1,24 +1,26 @@
 package leetcode.editor.cn;
 
+import java.util.HashMap;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int totalFruit(int[] fruits) {
         if (fruits.length == 1 || fruits.length == 2) return fruits.length;
-        int maxLength = 2;
-        int left = 0, right = 1;
-        while (right+1 < fruits.length) {
-            while (fruits[left] == fruits[right]) {
-                right++;
-                maxLength++;
+        int ret = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int left = 0, right = 0; right < fruits.length; right++) {
+            hashMap.put(fruits[right], hashMap.getOrDefault(fruits[right], 0) + 1);
+            if (hashMap.size() > 2) {
+                // 出窗口
+                hashMap.put(fruits[left], hashMap.get(fruits[left])-1);
+                if (hashMap.get(fruits[left]) == 0) {
+                    hashMap.remove(fruits[left]);
+                }
+                left++;
             }
-            if (fruits[right+1] == fruits[right] || fruits[right+1] == fruits[left]) {
-                right++;
-                maxLength++;
-            } else {
-                left++;right++;
-            }
+            ret = Math.max(ret, right - left + 1);
         }
-        return maxLength;
+        return ret;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
