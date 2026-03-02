4dcd3203421192293dd87154e36aaeeee9c587c5
mou-kexin <2584857586@qq.com>
2026-03-02 18:27:42 +0800

    力扣代码

---
+++ leetcode/editor/cn/[30]串联所有单词的子串.java
@@ -0,0 +1,38 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> newList = new ArrayList<>();
+        int len = words[0].length(), m = words.length;
+        HashMap<String, Integer> wordsHash = new HashMap<>();
+        for (String word : words) {
+            wordsHash.put(word, wordsHash.getOrDefault(word, 0)+1);
+        }
+        for (int i = 0; i < len; i++) {
+            HashMap<String, Integer> sHash = new HashMap<>();
+            for (int left = i, right = i, count = 0; right + len <= s.length() ; right+=len) {
+                String in = s.substring(right, right + len);
+                sHash.put(in, sHash.getOrDefault(in, 0) + 1);
+                if (sHash.get(in) <= wordsHash.getOrDefault(in, 0)) {
+                    count++;
+                }
+                if (right - left + 1 > len * m) {
+                    String out = s.substring(left, left + len);
+                    if (sHash.get(out) <= wordsHash.getOrDefault(out, 0)) {
+                        count--;
+                    }
+                    sHash.put(out, sHash.get(out) - 1);
+                    left += len;
+                }
+                if (count == m) newList.add(left);
+            }
+        }
+        return newList;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/cn/[438]找到字符串中所有字母异位词.java
+++ leetcode/editor/cn/[438]找到字符串中所有字母异位词.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution438 {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> newList = new ArrayList<>();
 
