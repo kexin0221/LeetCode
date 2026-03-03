36443e159579ec38277bf6deda3b8f5798fe8d3a
mou-kexin <2584857586@qq.com>
2026-03-03 17:28:30 +0800

    力扣代码

---
+++ leetcode/editor/cn/[76]最小覆盖子串.java
@@ -0,0 +1,44 @@
+package leetcode.editor.cn;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String minWindow(String ss, String tt) {
+        char[] s = ss.toCharArray();
+        char[] t = tt.toCharArray();
+
+        int[] hash1 = new int[128];
+        int kinds = 0;
+        for (char ch : t) {
+            if (hash1[ch]++ == 0) {
+                kinds++;
+            }
+        }
+
+        int[] hash2 = new int[128];
+        int minlen = Integer.MAX_VALUE, begin = -1;
+
+        for (int left = 0, right = 0, count = 0; right < s.length; right++) {
+            char in = s[right];
+            if(++hash2[in] == hash1[in]) {
+                count++;
+            }
+
+            while (count == kinds) {
+                if (right - left + 1 < minlen) {
+                    minlen = right - left + 1;
+                    begin = left;
+                }
+                char out = s[left++];
+                if (hash2[out]-- == hash1[out]) {
+                    count--;
+                }
+            }
+        }
+        if (begin == -1) {
+            return "";
+        } else {
+            return ss.substring(begin, begin + minlen);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
