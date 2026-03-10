e6ced1b327fe3566b19f53e41d311dd56fb24c31
mou-kexin <2584857586@qq.com>
2026-03-10 18:28:06 +0800

    力扣代码

--- leetcode/editor/cn/[35]搜索插入位置.java
+++ leetcode/editor/cn/[35]搜索插入位置.java
@@ -3,23 +3,19 @@ package leetcode.editor.cn;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution35 {
     public int searchInsert(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] < target && nums[i+1] > target) {
-                return i+1;
-            }
+        if (nums[left] < target) {
+            return left + 1;
         }
-        if (nums[nums.length - 1] < target) {
-            return nums.length;
-        }
-        if (nums[0] > target) {
-            return 0;
-        }
-        return -1;
+        return left;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
