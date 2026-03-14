d99d619a97a971b39140012df48dadf470aab8e7
mou-kexin <2584857586@qq.com>
2026-03-14 18:18:28 +0800

    力扣代码

--- leetcode/editor/cn/test.java
+++ leetcode/editor/cn/test.java
@@ -6,29 +6,25 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // 注意 hasNext 和 hasNextLine 的区别
-        // 注意 while 处理多个 case
-        int n = in.nextInt();
-        int m = in.nextInt();
-        long[] arr = new long[n];
-        int i = 0;
-        while (in.hasNextLong() && i < n) {
-            arr[i] = in.nextLong();
-            i++;
-        }
-        while (m > 0) {
-            int l = in.nextInt();
-            int r = in.nextInt();
-            Long[] dp = new Long[n];
-            dp[0] = arr[0];
-            for (int j = 1; j < dp.length; j++) {
-                dp[j] = dp[j-1] + arr[j];
+        int n = in.nextInt(); // 行数
+        int m = in.nextInt(); // 列数
+        int q = in.nextInt(); // 查询次数
+        int[][] arr = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = in.nextInt();
             }
-            Long sum;
-            if (l == 1) sum = dp[r-1];
-            else sum = dp[r-1] - dp[l-2];
-            System.out.println(sum);
-            m--;
+        }
+        long[][] dp = new long[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]+arr[i][j]-dp[i-1][j-1];
+            }
+        }
+        while (q > 0) {
+            int x1 = in.nextInt(), y1 = in.nextInt(), x2 = in.nextInt(), y2 = in.nextInt();
+            System.out.println(dp[x2][y2]-dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1]);
+            q--;
         }
     }
 }
