b43943be7cf9ed91f33cb67b53910a4ba6f2ab05
mou-kexin <2584857586@qq.com>
2026-03-17 18:23:48 +0800

    力扣代码

--- leetcode/editor/cn/[238]除了自身以外数组的乘积.java
+++ leetcode/editor/cn/[238]除了自身以外数组的乘积.java
@@ -4,14 +4,21 @@ package leetcode.editor.cn;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            answer[i] = 1;
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    answer[i] *= nums[j];
-                }
-            }
+        int[] f = new int[nums.length];
+        int[] g = new int[nums.length];
+
+        f[0] = g[nums.length - 1] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            f[i] = f[i-1] * nums[i-1];
         }
+        for (int i = nums.length-2; i >= 0; i--) {
+            g[i] = g[i+1] * nums[i+1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = f[i] * g[i];
+        }
+
         return answer;
     }
 }
