39c3646bb892033b5c2adef7ab0b56ee65e7d75b
mou-kexin <2584857586@qq.com>
2026-03-20 18:34:23 +0800

    力扣代码

--- leetcode/editor/cn/[525]连续数组.java
+++ leetcode/editor/cn/[525]连续数组.java
@@ -3,16 +3,14 @@ package leetcode.editor.cn;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findMaxLength(int[] nums) {
-        int maxLength = 0;
-        if (nums.length == 1) return 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) nums[i] = -1;
+        }
+        int maxLength = 0, sum = 0;
         for (int i = 0; i < nums.length-1; i++) {
-            int sum = nums[i];
+            sum += nums[i];
             for (int j = i+1; j < nums.length; j++) {
-                sum += nums[j];
-                int curLength = j - i + 1;
-                if (sum * 2 == curLength) {
-                    maxLength = Math.max(maxLength, curLength);
-                }
+
             }
         }
         return maxLength;
--- leetcode/editor/cn/[560]和为 K 的子数组.java
+++ leetcode/editor/cn/[560]和为 K 的子数组.java
@@ -1,20 +1,19 @@
 package leetcode.editor.cn;
 
+import java.util.HashMap;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution560 {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        for (int l = 0; l < nums.length; l++) {
-            int sum = nums[l];
-            if (sum == k) count++;
-            if (l+1 < nums.length) {
-                for (int r = l+1; r < nums.length; r++) {
-                    sum += nums[r];
-                    if (sum == k) count++;
-                }
-            }
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        hashMap.put(0, 1);
+        int sum = 0, ret = 0;
+        for (int num : nums) {
+            sum += num;
+            ret += hashMap.getOrDefault(sum - k, 0);
+            hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1);
         }
-        return count;
+        return ret;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
