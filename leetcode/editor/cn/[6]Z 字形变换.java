36954cbfade0f0b88a5a119c660c52bea3545177
mou-kexin <2584857586@qq.com>
2026-04-03 22:22:49 +0800

    力扣代码

--- leetcode/editor/cn/[6]Z 字形变换.java
+++ leetcode/editor/cn/[6]Z 字形变换.java
@@ -3,10 +3,22 @@ package leetcode.editor.cn;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String convert(String s, int numRows) {
-        int classNum = 2 * numRows - 2;
-        for (int i = 0; i < classNum; i++) {
+        if(numRows == 1) return s;
+        int d = 2 * numRows - 2, n = s.length();
+        StringBuilder ret = new StringBuilder();
 
+        for(int i = 0; i < n; i += d) ret.append(s.charAt(i));
+
+        for(int k = 1; k < numRows - 1; k++) {
+            for(int i = k, j = d - i; i < n || j < n; i += d, j += d) {
+                if(i < n) ret.append(s.charAt(i));
+                if(j < n) ret.append(s.charAt(j));
+            }
         }
+
+        for(int i = numRows - 1; i < n; i += d)
+            ret.append(s.charAt(i));
+        return ret.toString();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
