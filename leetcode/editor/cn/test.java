61bbfd199b2173db057dfc6489c5b2662a400f3d
mou-kexin <2584857586@qq.com>
2026-04-04 23:25:08 +0800

    力扣代码

--- leetcode/editor/cn/test.java
+++ leetcode/editor/cn/test.java
@@ -2,7 +2,5 @@ package leetcode.editor.cn;
 
 public class test {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.countAndSay(4));
     }
 }
