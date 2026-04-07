92c71534310876440a2a963bf0acb26bdcea9681
mou-kexin <2584857586@qq.com>
2026-04-07 21:19:19 +0800

    力扣代码

--- leetcode/editor/cn/[315]计算右侧小于当前元素的个数.java
+++ leetcode/editor/cn/[315]计算右侧小于当前元素的个数.java
@@ -1,21 +1,28 @@
 package leetcode.editor.cn;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution315 {
     public List<Integer> countSmaller(int[] nums) {
         List<Integer> retList = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            retList.add(mergeNums(nums, i));
+            int count = 0;
+            int[] tmp = Arrays.copyOfRange(nums,i+1, nums.length);
+            Arrays.sort(tmp);
+            int j = tmp.length-1;
+            while (j >= 0) {
+                if (tmp[j] < nums[i]) {
+                    count = j+1;
+                    break;
+                }
+                j--;
+            }
+            retList.add(count);
         }
         return retList;
-    }
-
-    public int mergeNums(int[] nums, int key) {
-        if (key == nums.length - 1) return 0;
-
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
